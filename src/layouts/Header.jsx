b6d98abd7a6f256edfb064da663e82423ffbe8d3
seod4423 <seod4423@gmail.com>
2023-08-07 21:51:44 +0900

    header reset

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,34 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 
 const Header = () => {
   return (
     <>
-      <HeaderComponent>
-        <HeaderDiv>
-          <div class='header'>Nexflix</div>
-          <div class='header'>
-            <select name='language'>
-              <option value='kor'>한국어</option>
-              <option value='eng'>English</option>
-            </select>
-          </div>
-          <div class='header'>
-            <button>로그인</button>
-          </div>
-        </HeaderDiv>
-      </HeaderComponent>
+      <header>header</header>
     </>
   );
 };
 
 export default Header;
-
-const HeaderComponent = styled.header`
-  width: 100vw;
-  height: 80px;
-`;
-
-const HeaderDiv = styled.div`
-  display: flex;
-`;
