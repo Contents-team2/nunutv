fbb8d0ae55b5599275d848034ed2b7b296485f05
seod4423 <seod4423@gmail.com>
2023-08-07 20:39:01 +0900

    footer layout

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
   return (
     <>
-      <header>header</header>
+      <HeaderComponent>
+        <HeaderDiv>
+          <div class='header'>Nexflix</div>
+          <div class='header'>
+            <select name='language'>
+              <option value='kor'>한국어</option>
+              <option value='eng'>English</option>
+            </select>
+          </div>
+          <div class='header'>
+            <button>로그인</button>
+          </div>
+        </HeaderDiv>
+      </HeaderComponent>
     </>
   );
 };
 
 export default Header;
+
+const HeaderComponent = styled.header`
+  width: 100vw;
+  height: 80px;
+`;
+
+const HeaderDiv = styled.div`
+  display: flex;
+`;
