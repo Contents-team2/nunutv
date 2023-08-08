ccfd0118bf7c1f19c673d89df96d0912d898206a
jumosd <jumosd@icloud.com>
2023-08-08 12:07:10 +0900

    FIX : main.jsx 이름변경 Main.jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Header from './layouts/Header';
 import Footer from './layouts/Footer';
-import Main from "./pages/main"
+import Main from "./pages/Main"
 
 
 const router = createBrowserRouter([
