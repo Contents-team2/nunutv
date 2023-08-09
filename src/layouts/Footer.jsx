e9fd78f0f7da1f91a1fb797fb58481a488eb8c91
seod4423 <seod4423@gmail.com>
2023-08-09 14:56:32 +0900

    footer layout done

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -60,14 +60,19 @@ const Footer = () => {
           </LinklistGrid>
           <LanguageSelectBox>
             <LanguageSelectButton>
-              <GlobleImg role='img'>
-                <img src='/images/icon/global_ico.svg' alt='global' />
-              </GlobleImg>
-              <Select name='LanguageSelect' id='asdf'>
-                <option value='ko'>한국어</option>
-                <option value='en'>영어</option>
-                한국어
-              </Select>
+              <LanguageSelectDiv>
+                <GlobleImg role='img'>
+                  <img src='/images/icon/global_ico_dark.svg' alt='global' />
+                </GlobleImg>
+                <Select name='LanguageSelect' id='asdf'>
+                  <option value='ko'>한국어</option>
+                  <option value='en'>영어</option>
+                  한국어
+                </Select>
+                <ArrowImg>
+                  <img src='/images/icon/arrow_bottom.svg' alt='global' />
+                </ArrowImg>
+              </LanguageSelectDiv>
             </LanguageSelectButton>
           </LanguageSelectBox>
           <BrandLocation>Nunu Korea</BrandLocation>
@@ -163,12 +168,31 @@ const LanguageSelectButton = styled.div`
   justify-content: space-between;
 `;
 
+const LanguageSelectDiv = styled.div`
+  position: relative;
+`;
+
 const GlobleImg = styled.div`
   position: absolute;
+  top: 6px;
+  left: 10px;
 `;
 
 const Select = styled.select`
-  left: 20px;
+  color: #fff;
+  background-color: rgba(22, 22, 22, 0.7);
+  padding: 6px 36px;
+  border-radius: 4px;
+  -webkit-appearance: none; /* 크롬 화살표 없애기 */
+  -moz-appearance: none; /* 파이어폭스 화살표 없애기 */
+  appearance: none; /* 화살표 없애기 */
+  text-align-last: center;
+`;
+
+const ArrowImg = styled.div`
+  position: absolute;
+  top: 6px;
+  left: 86px;
 `;
 
 const BrandLocation = styled.p`
