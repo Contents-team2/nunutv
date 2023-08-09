6fddd61ff42047d46ad6237cd9ff28832c5c4598
Joojue <128790356+Joojue@users.noreply.github.com>
2023-08-09 16:50:32 +0900

    반응형 css, hover 효과 적용

--- src/modal/Style.js
+++ src/modal/Style.js
@@ -29,17 +29,37 @@ export const Box = styled.div`
 export const Display = styled.div`
   display: flex;
   height: 30rem;
+  @media (max-width: 1023px) {
+    height: 15rem;
+  }
+  @media (max-width: 767px) {
+    height: 10rem;
+  }
 `;
 
 export const Logo = styled.img`
   width: 20rem;
+  @media (max-width: 1023px) {
+    width: 10rem;
+  }
+  @media (max-width: 767px) {
+    width: 8rem;
+  }
 `;
 
 export const NameAndPlayBox = styled.div`
   position: absolute;
-  margin-top: 16rem;
+  margin-top: 17rem;
   margin-left: 4rem;
   color: black;
+  @media (max-width: 1023px) {
+    margin-top: 8rem;
+    margin-left: 2rem;
+  }
+  @media (max-width: 767px) {
+    margin-top: 5rem;
+    margin-left: 0.5rem;
+  }
 `;
 
 export const NameBox = styled.div`
@@ -52,20 +72,37 @@ export const PlayBox = styled.div`
   display: flex;
   align-items: center;
   margin-top: 3rem;
+  @media (max-width: 1023px) {
+    margin-top: 1.5rem;
+  }
+  @media (max-width: 767px) {
+    margin-top: 1rem;
+  }
 `;
 
 export const PlayButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 120px;
-  height: 45px;
+  width: 8rem;
+  height: 3rem;
   border-radius: 4px;
   cursor: pointer;
   text-align: center;
   background-color: white;
+  font-size: 1rem;
   font-weight: 600;
   padding: 0px 10px;
+  @media (max-width: 1023px) {
+    width: 5rem;
+    height: 1.5rem;
+    font-size: 0.7rem;
+  }
+  @media (max-width: 767px) {
+    width: 3rem;
+    height: 1rem;
+    font-size: 0.3rem;
+  }
 `;
 
 export const VideoBox = styled.div`
@@ -76,6 +113,12 @@ export const VideoBox = styled.div`
   border-top-left-radius: 1rem;
   border-top-right-radius: 1rem;
   border: none;
+  @media (max-width: 1023px) {
+    height: 15rem;
+  }
+  @media (max-width: 767px) {
+    height: 10rem;
+  }
 `;
 
 export const ThumbVideo = styled.div`
@@ -110,6 +153,39 @@ export const CloseBtn = styled.button`
   z-index: 4;
   position: absolute;
   cursor: pointer;
+  @media (max-width: 1023px) {
+    font-size: 1.5rem;
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+  }
+  @media (max-width: 767px) {
+    top: 2%;
+    font-size: 0.5rem;
+    padding-left: 0.25rem;
+    padding-right: 0.25rem;
+  }
+`;
+
+export const MuteBtn = styled.img`
+  right: 10%;
+  width: 2.5rem;
+  right: 15%;
+  top: 50%;
+  z-index: 2;
+  position: absolute;
+  cursor: pointer;
+  transition: all 0.2s linear;
+  &:hover {
+    transform: scale(1.2);
+  }
+  @media (max-width: 1023px) {
+    width: 1.5rem;
+    top: 20%;
+  }
+  @media (max-width: 767px) {
+    width: 1rem;
+    top: 15%;
+  }
 `;
 
 export const Describe = styled.div`
@@ -118,12 +194,26 @@ export const Describe = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  @media (max-width: 1023px) {
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
+  @media (max-width: 767px) {
+    margin-left: 0.7rem;
+    margin-right: 0.7rem;
+  }
 `;
 
 export const VideoTierBox = styled.div``;
 
 export const VideoTier = styled.img`
   width: 1.5rem;
+  @media (max-width: 1023px) {
+    width: 1rem;
+  }
+  @media (max-width: 767px) {
+    width: 0.7rem;
+  }
 `;
 
 export const SummaryBox = styled.div`
@@ -133,26 +223,62 @@ export const SummaryBox = styled.div`
 export const Summary = styled.div`
   line-height: 2rem;
   font-size: 1rem;
+  @media (max-width: 1023px) {
+    line-height: 1rem;
+    font-size: 0.5rem;
+  }
+  @media (max-width: 767px) {
+    line-height: 1rem;
+    font-size: 0.3rem;
+  }
 `;
 
 export const ActorAndGenre = styled.div`
   margin-left: 2rem;
   width: 30%;
+  @media (max-width: 1023px) {
+    margin-left: 1rem;
+  }
+  @media (max-width: 767px) {
+    margin-left: 0.5rem;
+  }
 `;
 
 export const ActorBox = styled.div`
   margin-bottom: 1rem;
   font-size: 1rem;
+  @media (max-width: 1023px) {
+    font-size: 0.5rem;
+    margin-bottom: 0.5rem;
+  }
+  @media (max-width: 767px) {
+    font-size: 0.3rem;
+    margin-bottom: 0.3rem;
+  }
 `;
 
 export const GenreBox = styled.div`
   margin-bottom: 1rem;
   font-size: 1rem;
+  @media (max-width: 1023px) {
+    font-size: 0.5rem;
+    margin-bottom: 0.5rem;
+  }
+  @media (max-width: 767px) {
+    font-size: 0.3rem;
+    margin-bottom: 0.3rem;
+  }
 `;
 
 export const VideoList = styled.div`
   margin-top: 4rem;
   margin-bottom: 4rem;
+  @media (max-width: 1023px) {
+    margin-top: 2rem;
+  }
+  @media (max-width: 767px) {
+    margin-top: 1rem;
+  }
 `;
 
 export const ListHeader = styled.div`
@@ -162,16 +288,38 @@ export const ListHeader = styled.div`
   margin-left: 3em;
   margin-right: 3rem;
   margin-bottom: 1rem;
+  @media (max-width: 1023px) {
+    margin-left: 1rem;
+    margin-right: 1rem;
+    margin-bottom: 0.5rem;
+  }
+  @media (max-width: 767px) {
+    margin-left: 0.7rem;
+    margin-right: 0.7rem;
+    margin-bottom: 0.3rem;
+  }
 `;
 
 export const ListInfo = styled.div`
   color: white;
   font-size: 1.7rem;
+  @media (max-width: 1023px) {
+    font-size: 1.2rem;
+  }
+  @media (max-width: 767px) {
+    font-size: 0.8rem;
+  }
 `;
 
 export const ListTitle = styled.div`
   color: white;
   font-size: 1.2rem;
+  @media (max-width: 1023px) {
+    font-size: 1rem;
+  }
+  @media (max-width: 767px) {
+    font-size: 0.5rem;
+  }
 `;
 
 export const ListCardWrapper = styled.div`
@@ -188,22 +336,61 @@ export const ListCardWrapper = styled.div`
   &:hover {
     background-color: #333333;
   }
+  @media (max-width: 1023px) {
+    height: 5rem;
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+  @media (max-width: 767px) {
+    height: 8rem;
+    margin-left: 0.7rem;
+    margin-right: 0.7rem;
+    justify-content: space-between;
+    align-items: start;
+    margin-top: 2rem;
+  }
 `;
 
 export const CardId = styled.div`
   font-size: 1.5rem;
+  @media (max-width: 1023px) {
+    font-size: 1rem;
+  }
+  @media (max-width: 767px) {
+    font-size: 1.5rem;
+    margin-right: 0.5rem;
+  }
 `;
 
-export const CardThumbs = styled.img`
-  background-color: wheat;
+export const CardThumbs = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
   width: 8rem;
   height: 5rem;
   border-radius: 0.25rem;
+  @media (max-width: 1023px) {
+    width: 5.5rem;
+    height: 3rem;
+  }
+  @media (max-width: 767px) {
+    width: 5rem;
+    height: 2.5rem;
+    margin-right: 0.5rem;
+  }
 `;
 
 export const CardDescribe = styled.div`
   width: 60%;
   height: 5rem;
+  @media (max-width: 1023px) {
+    height: 5rem;
+  }
+  @media (max-width: 767px) {
+    width: 80%;
+    height: 1.7rem;
+  }
 `;
 
 export const CardDescribeHeader = styled.div`
@@ -211,24 +398,63 @@ export const CardDescribeHeader = styled.div`
   justify-content: space-between;
   align-items: center;
   margin-bottom: 0.5rem;
+  @media (max-width: 1023px) {
+    font-size: 0.8rem;
+    margin-bottom: 0.3rem;
+  }
+  @media (max-width: 767px) {
+    font-size: 0.5rem;
+    margin-bottom: 0.17rem;
+  }
 `;
 
 export const CardDescribeSummary = styled.div`
   font-size: 0.75rem;
   color: #a6a6a6;
   line-height: 1rem;
+  @media (max-width: 1023px) {
+    font-size: 1px;
+    margin-bottom: 0.3rem;
+  }
+  @media (max-width: 767px) {
+    font-size: 0.5rem;
+    margin-bottom: 0.17rem;
+    margin-top: 0.5rem;
+  }
 `;
 
 export const ModalVideoDetailWrapper = styled.div`
-  background-color: #181818;
   margin-left: 3em;
   margin-right: 3rem;
   padding-bottom: 4rem;
+  @media (max-width: 1023px) {
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+  @media (max-width: 767px) {
+    margin-left: 0.7rem;
+    margin-right: 0.7rem;
+  }
 `;
 
 export const ModalVideoDetailTitle = styled.div`
   font-size: 1.5rem;
   margin-bottom: 1.5rem;
+  @media (max-width: 1023px) {
+    font-size: 1rem;
+    margin-bottom: 1rem;
+  }
+  @media (max-width: 767px) {
+    font-size: 0.9rem;
+    margin-bottom: 0.7rem;
+  }
 `;
 
-export const ModalVideoDetailSummary = styled.div``;
+export const ModalVideoDetailSummary = styled.div`
+  @media (max-width: 1023px) {
+    font-size: 0.7rem;
+  }
+  @media (max-width: 767px) {
+    font-size: 0.5rem;
+  }
+`;
--- src/modal/component/VideoBox.jsx
+++ src/modal/component/VideoBox.jsx
@@ -4,15 +4,10 @@ import { ref, getDownloadURL } from "firebase/storage";
 import ReactPlayer from "react-player";
 import * as S from "../Style";
 
-const VideoBox = () => {
+const VideoBox = (props) => {
   const [url, setUrl] = useState(null);
-  const [isMute, setIsMute] = useState(true);
 
   const starsRef = ref(storage, "real-video.mp4");
-
-  const muteHandler = () => {
-    setIsMute((prevIsMute) => !prevIsMute);
-  };
 
   useEffect(() => {
     getDownloadURL(starsRef).then((url) => {
@@ -29,7 +24,7 @@ const VideoBox = () => {
           height="100%" // 플레이어 크기 (세로)
           loop={true}
           playing={true} // 자동 재생 on 사파리는 안됨
-          muted={isMute} // 음소거
+          muted={props.isMute} // 음소거
           light={false} // 플레이어 모드
           pip={true} // pip 모드 설정 여부
         />
