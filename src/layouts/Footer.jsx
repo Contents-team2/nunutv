1975e50043651ed7716a92044a680de4456e4d2b
seod4423 <seod4423@gmail.com>
2023-08-10 16:21:42 +0900

    Update Footer.jsx

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { styled } from "styled-components";
-import LanguageBox from "../Components/LanguageBox";
+import LanguageBox from "../components/LanguageBox";
 
 const Footer = () => {
   return (
